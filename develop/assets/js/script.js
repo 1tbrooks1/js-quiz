00a03782244213558124f8bf3f125ae5508148d2
tbrooks <tbrooks321@yahoo.com>
2021-12-11 13:15:30 -0500

    created HTML boilerplate and added var/functions in js

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,3 +1,10 @@
 var quizContainer = document.getElementById('quiz');
 var resultContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+
+function buildQuiz() {}
+
+function showResults() {}
+
+buildQuiz();
+
