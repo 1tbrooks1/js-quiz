400358db7197e0ee191be0f8a64320fd09f119d7
tbrooks <tbrooks321@yahoo.com>
2021-12-15 13:39:01 -0500

    created variables for quiz

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,8 +1,18 @@
+// Variables for quiz elements
 var startButton  = document.getElementById('start');
 var questionEl  = document.getElementById('question');
 var choicesEl = document.getElementById('choices');
 var submitButton = document.getElementById('submit');
-var timerEl = document.getElementById('timer');
+var timerEl = document.getElementById('countdown');
+var initialsEL = document.getElementById('initials');
+var feedback = document.getElementById('feedback');
+
+// Variables for timer and quiz questions
+var timerId;
+var countdown = 120;
+var currentQuestionIndex = 0;
+
+// Questions to be cycled through
 var quizQuestions = [
     {
         question: "blah blah blah",
@@ -43,4 +53,9 @@ function showResults() {}
 
 startQuiz();
 
+
+// Clicking the 'Begin Quiz' button starts the quiz
 startButton.addEventListener('click', startQuiz);
+
+// Clicking submit button stores initials and score
+submitButton.addEventListener('click', saveHighscore)
