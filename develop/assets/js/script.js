b37cb205e9e2b40cc5028344db20efb306e53551
tbrooks <tbrooks321@yahoo.com>
2021-12-15 14:47:45 -0500

    created function to cycle through questions and answers

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -10,7 +10,7 @@ var feedback = document.getElementById('feedback');
 // Variables for timer and quiz questions
 var timerId;
 var countdown = 120;
-var currentQuestionIndex = 0;
+var currentQuestion = 0;
 
 // Questions to be cycled through
 var quizQuestions = [
@@ -56,10 +56,52 @@ function startQuiz() {
         countdown--;
         timerEl.innerHTML = countdown;
     }, 1000);
-
-    if (countdown <= 0);
+    if (countdown <= 0); {
     stopQuiz();
+    }
+
+    grabQuestion();
+
 }
+
+function grabQuestion() {
+    // grab first question from array
+    var currentQuestion = quizQuestions[questionIndex];
+
+    // changes title to the question
+    questionEl.children[0].innerHTML = currentQuestion.title;
+
+    // removes question choices
+    while (choicesEl.hasChildNodes()) {
+        choicesEl.removeChild(choicesEl.lastChild);
+    }
+
+    // for loop to cycle through question choices
+    for (i = 0; i < currentQuestion.answers.length; i++) {
+        
+        // dynamically creates buttons for answers
+        var answerButton = document.createElement("button");
+        answerButton.innerHTML = currentQuestion.answers[i];
+
+        // shows answer options
+        choicesEl.appendChild(answerButton);
+    }
+        
+    // adds click event listener to each answer option
+    choicesEl.children[0].addEventListener("click", function(event) {
+        answerClick(choicesEl.children[0]);
+    });
+    choicesEl.children[1].addEventListener("click", function(event) {
+        answerClick(choicesEl.children[1]);
+    });
+    choicesEl.children[2].addEventListener("click", function(event) {
+        answerClick(choicesEl.children[2]);
+    });
+    choicesEl.children[3].addEventListener("click", function(event) {
+        answerClick(choicesEl.children[3]);
+    });
+}
+
 
 
 
