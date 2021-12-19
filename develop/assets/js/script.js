53b4a5975079169e7585d06dcaae06052202a5c8
tbrooks <tbrooks321@yahoo.com>
2021-12-18 19:35:18 -0500

    finished up more functions to save and show highscores

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -11,233 +11,180 @@ var optionB = document.getElementById("choiceB");
 var optionC = document.getElementById("choiceC");
 var optionD = document.getElementById("choiceD");
 var optionButton = document.querySelector("#choices");
+var highScoreScreen = document.getElementById("highscore-screen");
+var scoresList = document.getElementById("score-list");
 
 
 var questions = [
-    {
-        title: "blah",
-        a: "bleh",
-        b: "blad",
-        c: "blas",
-        d: "blaa",
-        answer: "bleh"
-    },
-    {
-        title: "joe byron",
-        a: "hey",
-        b: "now",
-        c: "hey",
-        d: "now",
-        answer: "hey"
-    },
-    {
-        title: "blaeff",
-        a: "blah",
-        b: "blah",
-        c: "blah",
-        d: "blah",
-        answer: "blah"
-    },
-    {
-        title: "bither",
-        a: "blah",
-        b: "blah",
-        c: "blah",
-        d: "blah",
-        answer: "blah"
-    },
-]
+  {
+    title: "blah",
+    a: "bleh",
+    b: "blad",
+    c: "blas",
+    d: "blaa",
+    answer: "bleh",
+  },
+  {
+    title: "joe byron",
+    a: "hey",
+    b: "now",
+    c: "hey",
+    d: "now",
+    answer: "hey",
+  },
+  {
+    title: "bing bong",
+    a: "blah",
+    b: "blah",
+    c: "blah",
+    d: "blah",
+    answer: "blah",
+  },
+  {
+    title: "take me out to dinner",
+    a: "blah",
+    b: "blah",
+    c: "blah",
+    d: "blah",
+    answer: "blah",
+  },
+];
 
 var currentQuestionIndex = 0;
 var timerId;
 var time = 120;
 
-var startQuiz = function() {
-    var startScreen = document.getElementById("start-screen");
-    startScreen.classList.add("hide");
+var startQuiz = function () {
+  var startScreen = document.getElementById("start-screen");
+  startScreen.classList.add("hide");
+  highScoreScreen.classList.add("hide");
 
-    questionsEl.classList.add("show");
+  questionsEl.classList.add("show");
 
-    timerId = setInterval(function() {
-        time--;
-        timerEL.innerHTML = time;
-        if (time <= 0) {
-            endQuiz();
-        }
-    }, 1000);
+  timerId = setInterval(function () {
+    time--;
+    timerEL.innerHTML = time;
+    if (time <= 0) {
+      endQuiz();
+    }
+  }, 1000);
 
-    getQuestion();
-}
+  getQuestion();
+};
 
-var getQuestion = function() {
-    
-    var currentQuestion = questions[currentQuestionIndex];
+var getQuestion = function () {
+  var currentQuestion = questions[currentQuestionIndex];
 
-    questionTitle.innerHTML = currentQuestion.title;
-    optionA.innerHTML = currentQuestion.a;
-    optionA.addEventListener("click", checkAnswerA);
-    
-    
-    questionTitle.innerHTML = currentQuestion.title;
-    optionB.innerHTML = currentQuestion.b;
-    optionB.addEventListener("click", checkAnswerB);
-    
-    questionTitle.innerHTML = currentQuestion.title;
-    optionC.innerHTML = currentQuestion.c;
-    optionC.addEventListener("click", checkAnswerC);
-    
-    questionTitle.innerHTML = currentQuestion.title;
-    optionD.innerHTML = currentQuestion.d;
-    optionD.addEventListener("click", checkAnswerD);
-}
+  questionTitle.innerHTML = currentQuestion.title;
+  optionA.innerHTML = currentQuestion.a;
+  optionA.addEventListener("click", checkAnswer);
 
-var checkAnswerA = function() { 
-    if (optionA.innerHTML != questions[currentQuestionIndex].answer) {
+  questionTitle.innerHTML = currentQuestion.title;
+  optionB.innerHTML = currentQuestion.b;
+  optionB.addEventListener("click", checkAnswer);
+
+  questionTitle.innerHTML = currentQuestion.title;
+  optionC.innerHTML = currentQuestion.c;
+  optionC.addEventListener("click", checkAnswer);
+
+  questionTitle.innerHTML = currentQuestion.title;
+  optionD.innerHTML = currentQuestion.d;
+  optionD.addEventListener("click", checkAnswer);
+};
+
+var checkAnswer = function (event) {
+  var optionClicked = event.target;
+
+  console.log(optionClicked.innerHTML);
+
+  if (optionClicked.innerHTML != questions[currentQuestionIndex].answer) {
     time -= 15;
-    feedbackEl.innerHTML = "Wrong!"
-    }
-    else {
-        feedbackEl.innerHTML = "Correct!"
-        currentQuestionIndex++;
-        getQuestion();
-        console.log(currentQuestionIndex);
-        if(currentQuestionIndex >= 3) {
-            endQuiz();
-         } else {
-             getQuestion();
-}
-}
-}
-var checkAnswerB = function() { 
-    if (optionB.innerHTML != questions[currentQuestionIndex].answer) {
-        time -= 15;
-        feedbackEl.innerHTML = "Wrong!";
-        }
-        else {
-            feedbackEl.innerHTML = "Correct!"
-            currentQuestionIndex++;
-            getQuestion();
-            console.log(currentQuestionIndex);
-            if(currentQuestionIndex >= 3) {
-                endQuiz();
-             } else {
-                 getQuestion();
-}
-}
-}
-var checkAnswerC = function() { 
-    if (optionC.innerHTML != questions[currentQuestionIndex].answer) {
-        time -= 15;
-        feedbackEl.innerHTML = "Wrong!";
-        }
-        else {
-            feedbackEl.innerHTML = "Correct!"
-            currentQuestionIndex++;
-            getQuestion();
-            console.log(currentQuestionIndex);
-            if(currentQuestionIndex >= 3) {
-                endQuiz();
-             } else {
-                 getQuestion();
-}
-}
-}
-var checkAnswerD = function() { 
-    if (optionD.innerHTML != questions[currentQuestionIndex].answer) {
-        time -= 15;
-        feedbackEl.innerHTML = "Wrong!"
-    }
-        else {
-            feedbackEl.innerHTML = "Correct!"
+    feedbackEl.innerHTML = "Wrong!";
+  } else {
+    feedbackEl.innerHTML = "Correct!";
+  }
 
-            currentQuestionIndex++;
-            
-            getQuestion();
-            
-            console.log(currentQuestionIndex);
-            
-            if(currentQuestionIndex >= 3) {
-                endQuiz();
-             } else {
-                 getQuestion();
-}
-}
-}
+  feedbackEl.setAttribute("class", "feedback");
+  setInterval(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 750);
 
+  currentQuestionIndex++;
 
-/*var questionClick = function(answer) {
-    
-    if (answer != questions[currentQuestionIndex].answer) {
-
-        time -= 15;
-
-        feedbackEl.innerHTML = "Wrong!";
-    }
-    else {
-        feedbackEl.innerHTML = "Correct!";
-    }
-
-    feedbackEl.setAttribute("class", "feedback");
-    setInterval(function() {
-        feedbackEl.setAttribute("class", "feedback hide");
-    }, 1000);
-
-    currentQuestionIndex++;
-
-    if(currentQuestionIndex === questions.length) {
-        endQuiz();
-     } else {
-         getQuestion();
-     }
-}*/
+  if (currentQuestionIndex === 4) {
+    endQuiz();
+  } else {
+    getQuestion();
+  }
+};
 
 var endQuiz = function () {
-    questionTitle.classList.add("hide");
-    optionA.classList.add("hide");
-    optionB.classList.add("hide");
-    optionC.classList.add("hide");
-    optionD.classList.add("hide");
-    
+  questionTitle.classList.add("hide");
+  optionA.classList.add("hide");
+  optionB.classList.add("hide");
+  optionC.classList.add("hide");
+  optionD.classList.add("hide");
 
-    clearInterval(timerId);
-    timerEL.innerHTML = time;
-    
+  clearInterval(timerId);
+
+  timerEL.innerHTML = time;
+
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.setAttribute("class", " ");
+
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.innerHTML = time;
+};
+
+var saveHighScore = function () {
+  var initials = initialsEl.value;
+  var highScores;
+
+  if (initials.length === 0 || initials.length > 3) {
+    alert("Try again! Please enter your initials between 2 - 3 characters!");
+  } else {
+    if (JSON.parse(localStorage.getItem("highScores")) != null)
+      highScores = JSON.parse(window.localStorage.getItem("highScores"));
+    else highScores = [];
+
+    var newScore = {
+      initials: initials,
+      score: time,
+    };
+
+    highScores.push(newScore);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    displayHighScore();
+  }
+};
+
+var displayHighScore = function () {
+    highScores = JSON.parse(localStorage.getItem("highScores"));
     var endScreenEl = document.getElementById("end-screen");
-    endScreenEl.setAttribute("class", " ");
+    endScreenEl.classList.add("hide");
+    highScoreScreen.classList.add("show");
 
-    var finalScoreEl = document.getElementById("final-score");
-    finalScoreEl.innerHTML = time;
+    if(highScores != null){
+        var listOfScores = document.createElement("ol")
+        listOfScores.className = "score-list";
 
-    questionsEl.setAttribute("class", "hide");
-}
-
-var saveHighscore = function () {
-    var initials = initialsEl.value;
-
-    if (initials === " " || initials > 3) {
-        alert("Try again! Please enter your initials between 2 - 3 characters!");
+    for(i = 0; i < highScores.length; i++){
+        // create li tag for each high score
+        var scoreLi = document.createElement("li");
+        scoreLi.textContent = highScores[i].initials + " - " + highScores[i].score;
+        // display on page
+        document.getElementById("score-list").appendChild(scoreLi);
+      }
+      
     }
-    else {
-        var highscores;
-        if (JSON.parse(localStorage.getItem("highscores")) != null)
-            highscores = JSON.parse(window.localStorage.getItem("highscores"));
-        else
-        highscores = [];
-        
-        var newScore = {
-            initials: initials,
-            score: time
-        };
 
-        highscores.push(newScore);
 
-        localStorage.setItem("highscores", JSON.stringify(highscores));
 
-        location.href = "highscores.html"
-}
-}
+
+};
 
 startButton.addEventListener("click", startQuiz);
 
-submitButton.addEventListener("click", saveHighscore);
-
+submitButton.addEventListener("click", saveHighScore);
